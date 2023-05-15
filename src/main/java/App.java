528e1cfbe0bdda8dbb644f7199a681530cd722df
Rustem <grimhuman1@gmail.com>
2023-05-15 19:38:52 +0300

    PP_2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,7 +6,11 @@ public class App {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+                applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+        Cat catBean1 = applicationContext.getBean("cat", Cat.class);
+        Cat catBean2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println("Сравнение helloWorld " + (bean == bean2));
+        System.out.println("Сравнение cat " + (catBean1 == catBean2));
     }
 }
